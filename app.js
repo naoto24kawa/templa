686914ce73707665c8e8eaffaaf0120def78599f
Naoto Nishikawa <nishikawa_naoto@magus-inc.co.jp>
2018-03-06 12:56:32 +0900

    add multi data array

--- app.js
+++ app.js
@@ -29,6 +29,17 @@ $(document).ready(function() {
         for(let i = 0; i < splited.length; i++) {
           values[i] = splited[i].split(','); // カンマで分割し、配列に詰める
         }
+        // ヘッダー長と一致しない、要素の削除
+        const headerLength = values[0].length;
+        let indexNum = []; // 削除対象の要素の番号を保持する
+        for (let i = 1; i < values.length; i++) {
+          if(values[i].length != headerLength) {
+            indexNum.unshift(i);
+          }
+        }
+        for (let i = 0; i < indexNum.length; i++) {
+          values.splice(indexNum[i], 1);
+        }
         console.log('split values');
         console.log(values);
       }
@@ -37,14 +48,21 @@ $(document).ready(function() {
 
   $('#execute').on({
     'click': function(e){
+      let regexps = [];
       let result = [];
 
-      for (let i = 0; i < values[0].length; i++) {
-        let escaped = values[0][i].replace(/[\\^$.*+?()[\]{}|]/g, '\\$&');
-        const regexp = new RegExp(escaped, 'g');
-        let result = template.replace(regexp, values[1][i]);
+      for (var i = 0; i < values[0].length; i++) {
+        regexps[i] = new RegExp(values[0][i].replace(/[\\^$.*+?()[\]{}|]/g, '\\$&'), 'g');
+      }
 
-        let blob = new Blob([result], {type: 'text/plain'}); // バイナリデータを作ります
+      for (let i = 1; i < values.length; i++) {
+        result[i] = template;
+        for (var j = 0; j < values[i].length; j++) {
+          result[i] = result[i].replace(regexps[j], values[i][j]);
+        }
+
+        // バイナリデータを作ります
+        let blob = new Blob([result[i]], {type: 'text/plain'});
         // IEか他ブラウザかの判定
         if(window.navigator.msSaveBlob) {
             // IEなら独自関数を使います。
